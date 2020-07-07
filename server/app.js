74d662ab7cd460eded85f8bee7f506067a2a8cb2
Alan <alanalvarez98.aa@gmail.com>
2020-07-07 12:58:53 -0500

    Devdependencies

--- server/app.js
+++ server/app.js
@@ -1,10 +1,37 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hola');
+
+import morgan from 'morgan'
+import cors from 'cors'
+import path from 'path'
+
+
+const history = require('connect-history-api-fallback');
+
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(history());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static(path.join(__dirname, 'public/')));
+
+// app.get('/', (req, res) => {
+//     res.send('Hola');
+// });
+
+app.set('puerto', process.env.PORT || 3000);
+
+app.listen(app.get('puerto'), () => {
+    console.log('Servirdor corriendo en el puerto ' + app.get('puerto'));
 });
 
-app.listen(3000, () => {
-    console.log('Servirdor corriendo');
-});
+// npm install -g nodemon
+// npm run dev
+// npm i morgan --save
+// npm i cors --save
+// npm install --save connect-history-api-fallback
+// npm install -D @babel/core @babel/cli @babel/preset-env @babel/node
+// npm install mysql
