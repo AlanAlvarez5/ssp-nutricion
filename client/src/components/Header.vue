19391d3c6a1adb01a8a279cc0a22010bcb0710fd
Alan <alanalvarez98.aa@gmail.com>
2020-07-09 13:17:37 -0500

    Vista de login y header

---
+++ client/src/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+     <div>
+          <v-app-bar
+               color="primary"
+               dense
+               dark
+          >
+               <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+
+               <v-toolbar-title>SISE Nutricion</v-toolbar-title>
+
+               <v-spacer></v-spacer>
+
+               <v-btn tex-icon dark color="error">
+                    Salir
+                    <v-icon class="ml-3">fas fa-sign-out-alt</v-icon>
+               </v-btn>
+
+               <!-- <v-menu
+               left
+               bottom
+               >
+               <template v-slot:activator="{ on, attrs }">
+               <v-btn
+                    icon
+                    v-bind="attrs"
+                    v-on="on"
+               >
+                    <v-icon>mdi-dots-vertical</v-icon>
+               </v-btn>
+               </template>
+
+               <v-list>
+               <v-list-item
+                    v-for="n in 5"
+                    :key="n"
+                    @click="() => {}"
+               >
+                    <v-list-item-title>Option {{ n }}</v-list-item-title>
+               </v-list-item>
+               </v-list>
+               </v-menu> -->
+          </v-app-bar>
+     </div>
+</template>
+
+<script>
+export default {
+     name: 'Header',
+
+}
+</script>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,50 @@
+<template>
+          <v-row align="center" justify="center">
+               <v-col align="center">
+                    <v-card
+                    class="mx-auto px-5 py-5"
+                    max-width="344"
+                    >
+                         <v-card-text>
+                              <h1>Iniciar Sesión</h1>
+                         </v-card-text>
+                         <v-card-actions>
+                              <v-text-field v-model="correo" label="Correo Insitucional" :rules="emailRules" hide-details="auto"></v-text-field>
+                         </v-card-actions>
+                         <v-card-actions>
+                              <v-text-field type="password" v-model="password" label="Contraseña" :rules="rules" hide-details="auto"></v-text-field>
+                         </v-card-actions>
+                         <v-btn  :disabled="disabled" class="my-3" block small color="primary">Iniciar Sesión</v-btn>
+                    </v-card>
+               </v-col>
+          </v-row>
+</template>
+
+<script>
+export default {
+     name: 'Login',
+     data: () => ({
+          rules: [
+               value => !!value || 'Requerido',                                                                
+          ],
+          emailRules: [ 
+               value => !!value || 'Requerido',
+               value => !value || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) || 'El correo debe de ser valido'
+
+          ],
+          correo: '',
+          password: '',
+
+     }),
+     computed: {
+          disabled: function () {
+               
+               if (this.password === '' || this.correo === '' || !(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(this.correo))){
+                    return true
+               } else {
+                    return false
+               }
+          }
+     }
+}
+</script>
